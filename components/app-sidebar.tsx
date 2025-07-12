c58ed59a8d484d6290eed5f3b8e7966759c8a967
shaan-m0 <shaanalam369@gmail.com>
2025-07-13 02:15:02 +0530

    added user options in sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,11 +1,23 @@
 "use client";
 import { Gradient } from "whatamesh";
-
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Building2,
+  Calendar,
+  ChevronUp,
+  Home,
+  Inbox,
+  LogOut,
+  Search,
+  Settings,
+  User,
+  User2,
+} from "lucide-react";
 
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
@@ -16,6 +28,17 @@ import {
 import Image from "next/image";
 import { useEffect } from "react";
 import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { useUser } from "@clerk/nextjs";
 
 const items = [
   {
@@ -47,6 +70,8 @@ const items = [
 ];
 
 const AppSidebar = () => {
+  const { user } = useUser();
+
   useEffect(() => {
     const gradient = new Gradient();
     gradient.initGradient("#gradient-canvas");
@@ -94,6 +119,59 @@ const AppSidebar = () => {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <Avatar>
+                    <AvatarImage src={user?.imageUrl} />
+                    <AvatarFallback>
+                      {user?.firstName?.at(0)?.toUpperCase()}{" "}
+                      {user?.lastName?.at(0)?.toUpperCase() || ""}
+                    </AvatarFallback>
+                  </Avatar>
+                  {user?.firstName} {user?.lastName!}
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                className="w-[--radix-popper-anchor-width]"
+              >
+                <DropdownMenuItem>
+                  <User className="mr-2 h-4 w-4" />
+                  <span>Profile</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Settings className="mr-2 h-4 w-4" />
+                  <span>Settings</span>
+                </DropdownMenuItem>
+                <DropdownMenuSub>
+                  <DropdownMenuSubTrigger>
+                    <Building2 className="mr-2 h-4 w-4" />
+                    <span>Change Workspace</span>
+                  </DropdownMenuSubTrigger>
+                  <DropdownMenuSubContent>
+                    <DropdownMenuItem>
+                      <span>Workspace 1</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <span>Workspace 2</span>
+                    </DropdownMenuItem>
+                  </DropdownMenuSubContent>
+                </DropdownMenuSub>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <LogOut className="mr-2 h-4 w-4" />
+                  <span>Logout</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 };
